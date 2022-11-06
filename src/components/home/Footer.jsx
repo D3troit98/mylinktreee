2d849031c51bf5612bae945cd478da4337b36cc0
D3troit98 <duruakuebuka@gmail.com>
2022-11-06 21:19:42 +0100

    redirect file added

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -3,6 +3,7 @@ import { zuri, i4g } from "../../assets";
 const Footer = () => {
   return (
     <footer>
+    
       <img src={zuri} alt="zuri logo" className="zuri-logo" />
       <p className="footer-text">HNG Internship 9 Frontend Task</p>
       <img src={i4g} alt="i4g logo" className="i4g-logo" />
